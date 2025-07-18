df95f75dde12e4ebd80c0db7fdfcd29af16444e6
jason <jasonxuuuu@gmail.com>
2025-07-18 22:33:37 +0800

    feat: created function to register user to the db

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.cli import load_dotenv
 
 from database import PostgresHandler
@@ -28,10 +28,48 @@ def ping_db():
         db.execute("SELECT 1")
 
         return jsonify({
-            'status': 'ok',
+            'status': 'success',
             'message': 'Database connection successful',
         }), 200
     except Exception as e:
+        return jsonify({
+            'status': 'fail',
+            'message': str(e),
+        }), 500
+    finally:
+        db.close()
+
+@app.route('/api/register', methods=['POST'])
+def register_user():
+    import bcrypt
+    data = request.get_json()
+
+    if not data or not all(key in data for key in ['username', 'password', 'email']):
+        return jsonify({
+            'status': 'fail',
+            'message': "Missing data. 'username', 'email', and 'password' are required.",
+        }), 400
+
+    username = data['username']
+    hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+    email = data['email']
+
+    db = create_db_connection()
+    try:
+        db.execute(
+            "INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)",
+            (username, email, hashed_password))
+        return jsonify({
+            'status': 'success',
+            'message': 'User registered successfully',
+        }), 201
+
+    except Exception as e:
+        if "unique constraint" in str(e).lower():
+            return jsonify({
+                'status': 'fail',
+                'message': "Email or Username already exists. Please try another one.",
+            }), 400
         return jsonify({
             'status': 'fail',
             'message': str(e),
--- backend/schema.sql
+++ backend/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    username TEXT,
+    username TEXT UNIQUE NOT NULL,
     avatar_url TEXT,
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
