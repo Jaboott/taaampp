fb7fdc1081343b77ed0bd15a6304c401fff6c763
jason <jasonxuuuu@gmail.com>
2025-07-24 14:51:52 +0800

    feat: added anime and anime_details tables to schema

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,3 +1,24 @@
+CREATE TYPE fuzzy_date AS (
+    year INTEGER,
+    month INTEGER,
+    day INTEGER
+);
+
+CREATE TYPE airing_schedule AS (
+    episode INTEGER NOT NULL,
+    airing_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE TYPE recommendation AS (
+    id INTEGER,
+    likes INTEGER
+);
+
+CREATE TYPE score_distribution AS (
+    score INTEGER,
+    amount INTEGER
+);
+
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email TEXT UNIQUE NOT NULL,
@@ -7,3 +28,34 @@ CREATE TABLE users (
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
+CREATE TABLE anime (
+    id INTEGER PRIMARY KEY,
+    titles TEXT[] NOT NULL,
+    format TEXT,
+    status TEXT NOT NULL,
+    season TEXT,
+    season_year INTEGER,
+    episodes INTEGER,
+    cover_image TEXT,
+    genres TEXT[],
+    average_score INTEGER,
+    studio TEXT[],
+    last_updated TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE anime_details (
+    id INTEGER PRIMARY KEY REFERENCES anime ON DELETE CASCADE,
+    description TEXT,
+    start_date fuzzy_date,
+    end_date fuzzy_date,
+    duration INTEGER,
+    source TEXT,
+    trailer TEXT,
+    banner_image TEXT,
+    popularity INTEGER NOT NULL DEFAULT 0,
+    trending INTEGER NOT NULL DEFAULT 0,
+    favourites INTEGER NOT NULL DEFAULT 0,
+    airing_schedule airing_schedule,
+    recommendations recommendation[],
+    score_distribution score_distribution
+);
